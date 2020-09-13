5a354bf113831ce6c2429da8d8b87a373aa01415
daniel <daniel@kimetrics.com>
2020-09-12 19:54:48 -0500

    Adding tabs components

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import {Image} from 'react-native'
 import {NavigationContainer} from '@react-navigation/native';
-
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import CoinsStack from './src/components/coins/CoinsStack';
+import Colors from './src/res/colors'
+
+const Tabs = createBottomTabNavigator()
 
 const App = () => {
     return (
         <>
             <NavigationContainer>
-                <CoinsStack />
+                <Tabs.Navigator
+                    tabBarOptions={{
+                        tintColor: "#fefefe",
+                        style: {
+                            backgroundColor: Colors.blackPearl
+                        }
+                    }}
+                >
+                    <Tabs.Screen
+                        name='coins'
+                        component={CoinsStack}
+                        options={{
+                            tabBarIcon: ({color, size}) => {
+                                return <Image
+                                    style={{tintColor: color, width: size, height: size}}
+                                    source={require('./src/assets/bank.png')}
+                                />
+                            }
+                        }}
+                    />
+                </Tabs.Navigator>
+
             </NavigationContainer>
         </>
     );
